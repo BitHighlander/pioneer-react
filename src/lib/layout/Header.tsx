5d539410cbf8cd063bf481c41e4ec99765a7500c
bithighlander <bithighlander@gmail.com>
2023-05-23 22:24:15 -0500

    dark mode

--- src/lib/layout/Header.tsx
+++ src/lib/layout/Header.tsx
@@ -1,8 +1,28 @@
 import { Box, Flex } from "@chakra-ui/react";
-
+import { useContext, useEffect, useState } from "react";
 import ThemeToggle from "./ThemeToggle";
 
 const Header = () => {
+  const onStart = async function () {
+    try {
+      // if(!wallet)
+      //   await connect();
+      //set color mode dark
+      localStorage.setItem("chakra-ui-color-mode", "dark");
+      // eslint-disable-next-line no-console
+
+    } catch (e) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      // eslint-disable-next-line no-console
+      console.error("header e: ", e);
+      // setKeepKeyError("Bridge is offline!");
+    }
+  };
+  useEffect(() => {
+    onStart();
+  }, []); // once on startup
+  
   return (
     <Flex
       as="header"

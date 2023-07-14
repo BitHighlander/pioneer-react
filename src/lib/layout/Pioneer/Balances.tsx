60f1bc46bada05f53fd50dd295ce485eea02a0fa
bithighlander <bithighlander@gmail.com>
2023-07-13 19:21:04 -0600

    pre-modal

--- src/lib/layout/Pioneer/Balances.tsx
+++ src/lib/layout/Pioneer/Balances.tsx
@@ -1,35 +1,56 @@
-import React, { useState } from 'react';
-import {
-    Avatar,
-    AvatarBadge,
-    Box,
-    Button,
-    HStack,
-    Stack,
-} from "@chakra-ui/react";
-import {usePioneer} from "lib/context/Pioneer";
+import React, { useState, useEffect } from 'react';
+import { Avatar, AvatarBadge, Box, Button, HStack, Stack, Image } from "@chakra-ui/react";
+import { usePioneer } from "lib/context/Pioneer";
+
 interface Balance {
     image?: string;
     symbol: string;
     balance?: any;
     size?: string;
+    context?: string; // Added context field
 }
-// @ts-ignore
+
 import KEEPKEY_ICON from "lib/assets/png/keepkey.png";
-// @ts-ignore
 import METAMASK_ICON from "lib/assets/png/metamask.png";
-// @ts-ignore
 import PIONEER_ICON from "lib/assets/png/pioneer.png";
 
-export default function Balances(balances: any) {
-    const {state, dispatch} = usePioneer();
-    const {api, user, context, wallets} = state;
+const getWalletType = (user: { walletDescriptions: any[] }, context: any) => {
+    if (user && user.walletDescriptions) {
+        const wallet = user.walletDescriptions.find((w) => w.id === context);
+        return wallet ? wallet.type : null;
+    }
+    return null;
+};
+
+const getWalletBadgeContent = (walletType: string) => {
+    const icons: any = {
+        metamask: METAMASK_ICON,
+        keepkey: KEEPKEY_ICON,
+        native: PIONEER_ICON,
+    };
+
+    const icon = icons[walletType];
+
+    if (!icon) {
+        return null;
+    }
+
+    return (
+        <AvatarBadge boxSize="1.25em" bg="green.500">
+            <Image rounded="full" src={icon} />
+        </AvatarBadge>
+    );
+};
+
+export default function Balances(balances: { balances: Balance[] }) {
+    const { state, dispatch } = usePioneer();
+    const { user } = state;
     const [currentPage, setCurrentPage] = useState(1);
     const balancesPerPage = 3;
     const indexOfLastBalance = currentPage * balancesPerPage;
     const indexOfFirstBalance = indexOfLastBalance - balancesPerPage;
-    const filteredBalances = balances.balances.filter((balance: any) => balance.balance !== 0);
-    const sortedBalances = filteredBalances.sort((a: any, b: any) => b.balance - a.balance);
+    const filteredBalances = balances.balances.filter((balance: Balance) => balance.balance !== 0);
+    const sortedBalances = filteredBalances.sort((a: Balance, b: Balance) => b.balance - a.balance);
     const currentBalances = sortedBalances.slice(indexOfFirstBalance, indexOfLastBalance);
     const totalPages = Math.ceil(sortedBalances.length / balancesPerPage);
 
@@ -38,33 +59,57 @@ export default function Balances(balances: any) {
     };
 
     const handleSendClick = (symbol: string) => {
-        // Handle send click logic here
         console.log(`Send clicked for balance: ${symbol}`);
     };
 
     const handleReceiveClick = (symbol: string) => {
-        // Handle receive click logic here
         console.log(`Receive clicked for balance: ${symbol}`);
     };
 
     const handleViewClick = (symbol: string) => {
-        // Handle view click logic here
         console.log(`View clicked for balance: ${symbol}`);
     };
 
+    useEffect(() => {
+        const setUser = async () => {
+            try {
+                if (user && user.wallets) {
+                    const { walletDescriptions, balances } = user;
+                    console.log("walletDescriptions: ", walletDescriptions);
+                    const updatedBalances = balances.map((balance: Balance) => {
+                        const walletType = getWalletType(user, balance.context);
+                        const badgeContent = getWalletBadgeContent(walletType);
+                        // @ts-ignore
+                        return {
+                            ...balance,
+                            context: {
+                                ...balance.context,
+                                badge: badgeContent,
+                            },
+                        };
+                    });
+                    dispatch({ type: 'SET_BALANCES', payload: updatedBalances });
+                }
+            } catch (e) {
+                console.error("Error: ", e);
+            }
+        };
+
+        setUser();
+    }, [user]);
+
     return (
         <Stack spacing={4}>
-            {currentBalances.map((balance: any, index: number) => (
+            {currentBalances.map((balance: Balance, index: number) => (
                 <Box key={index}>
                     <HStack spacing={4} alignItems="center">
                         <Avatar src={balance.image}>
                             <Box position="relative">
-                                <AvatarBadge boxSize="1em" bg="green.500" position="absolute" top="50%" right={0} />
-                                <AvatarBadge boxSize="1em" bg="blue.500" position="absolute" right={0} />
+                                {balance.context && balance.context.badge}
                             </Box>
                         </Avatar>
                         <Box>
-                            <small>asset: {balance.asset}</small>
+                            <small>asset: {balance.symbol}</small>
                             <br />
                             <small>balance: {balance.balance}</small>
                         </Box>

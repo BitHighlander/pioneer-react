374b921a62f52391f8e1575b9275031d137a1446
¨sktbrd¨ <sktbrd.eth@gmail.com>
2023-08-13 18:18:58 -0300

    navbar style

--- src/lib/layout/index.tsx
+++ src/lib/layout/index.tsx
@@ -11,16 +11,19 @@ type LayoutProps = {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
+    <div>
+        <Header />
     <Box margin="0 auto" maxWidth={800} transition="0.5s ease-out">
       <Meta />
       <Flex wrap="wrap" margin="8" minHeight="90vh">
-        <Header />
         <Box width="full" as="main" marginY={22}>
           {children}
         </Box>
         <Footer />
       </Flex>
     </Box>
+    </div>
+
   );
 };
 
